6fe3f537ce385a62ac5b2830441f51973fe01c9f
Some Guy <abc@abc.com>
2015-10-11 21:30:07 -0700

    Tests for demon words

--- test_demon_words.py
+++ test_demon_words.py
@@ -1,7 +1,16 @@
-from mystery_word import *
-
+from demon_words import *
 words = ['ALLY', 'BETA', 'COOL', 'DEAL', 'ELSE', 'FLEW', 'GOOD', 'HOPE', 'IBEX']
 
-def test_create_word_families()
-    assert test_create_word_families(words, 'A', []) == \
-           {'A---': ['ALLY'], '----': ['COOL', 'ELSE', 'FLEW', 'GOOD', 'HOPE', 'IBEX'], '---A': ['BETA'], '--A-': ['DEAL']}
+
+def test_create_word_families():
+    assert create_word_families(words, 'A', []) == \
+        {'A---': ['ALLY'], '----': ['COOL', 'ELSE', 'FLEW', 'GOOD', 'HOPE', 'IBEX'],
+            '---A': ['BETA'], '--A-': ['DEAL']}
+
+
+def test_find_max_family():
+    word_families = {'----': ['COOL', 'GOOD'], '---E': ['ELSE', 'HOPE']}
+    assert find_max_family(word_families, 'E') == '----'
+
+    word_families2 = {'----': ['COOL', 'GOOD'], '---E': ['ELSE']}
+    assert find_max_family(word_families2, 'W') == '----'
